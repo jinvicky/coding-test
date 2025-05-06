dcc2e6435e567dbc3e033bcc34234c4db16a1ea9
jinvicky <73690018+jinvicky@users.noreply.github.com>
2025-05-06 16:17:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 104.87 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=java) 
 
 ### 성능 요약
 
-메모리: 83.1 MB, 시간: 87.63 ms
+메모리: 113 MB, 시간: 104.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 11:26:05
+2025년 05월 06일 16:17:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,29 +1,20 @@
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 class Solution {
-      public String solution(String number, int k) {
+    public String solution(String number, int k) {
         Stack<Character> stack = new Stack<>();
-
-        for (char c : number.toCharArray()) {
-            // stack이 비어있지 않으며, 숫자가 stack의 마지막 값보다 크고 k가 남아있다면
-            while (k > 0 && !stack.isEmpty() && stack.peek() < c) {
-                stack.pop();  // 작은 값을 제거
-                k--;  // 제거된 숫자만큼 k 감소
+        for(char c : number.toCharArray()) {
+            while (k > 0 && !stack.isEmpty() && c > stack.peek()) {
+                stack.pop();
+                k--;
             }
-            stack.push(c);  // 현재 숫자를 스택에 추가
+            stack.push(c);
         }
-
-        // k가 아직 남아있다면, 스택에서 남은 숫자들을 제거
-        while (k > 0) {
+        
+        while(k-- > 0) {
             stack.pop();
-            k--;
         }
-
-        // 스택에 남은 숫자들을 결과 문자열로 변환
-        StringBuilder sb = new StringBuilder();
-        for (char ch : stack) {
-            sb.append(ch);
-        }
-
-        return sb.toString();
+        
+        return stack.stream().map(String::valueOf).collect(Collectors.joining());
     }
 }
