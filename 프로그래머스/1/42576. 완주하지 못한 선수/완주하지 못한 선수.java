49e75cb647f903ef8932fe67971d9e1f053441c8
jinvicky <73690018+jinvicky@users.noreply.github.com>
2026-01-19 18:29:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.15 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,23 +5,21 @@ class Solution {
         String answer = "";
         
         Map<String, Integer> map = new HashMap<>();
-       // map에 참가자들을 저장
+        
         for(String p : participant) {
-            int count = map.get(p) == null ? 1 : (int) map.get(p)+1;
-            map.put(p, count);
+            map.put(p, map.getOrDefault(p, 0) +1);
         }
         
-        // 완주자들을 map에서 카운터 감소 
+        
         for(String c : completion) {
-            int count = (int) map.get(c) -1;
-            map.put(c, count);
+            map.put(c, map.getOrDefault(c, 1)-1);
         }
         
-        for(Map.Entry<String, Integer> m : map.entrySet()) {
-            if(m.getValue() != 0) {
-                return m.getKey();
-            }
+        
+        for(Map.Entry<String, Integer> entry: map.entrySet()) {
+            if(entry.getValue() > 0) return entry.getKey();
         }
+        
         
         return answer;
     }
