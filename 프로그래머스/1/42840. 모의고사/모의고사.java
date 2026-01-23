afa2e8dc987652060698da4a51a4c493227b0c91
jinvicky <73690018+jinvicky@users.noreply.github.com>
2026-01-23 20:27:10 +0900

    [level 1] Title: 모의고사, Time: 4.02 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,75 +6,30 @@ class Solution {
         int[] patt1 = new int[]{1,2,3,4,5};
         int[] patt2 = new int[]{2,1,2,3,2,4,2,5};
         int[] patt3 = new int[]{3,3,1,1,2,2,4,4,5,5};
-        int maxOCnt = 0; // 최대 맞힌 갯수
         
-        int biggest = -999;
+        // 점수 배열 
+        int[] scores = new int[3];
         
-        // 트리맵을 만들어서 { 맞힌 개수 : 수포자 리스트 }
-        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
-        
-        // 
-        int index = 0;
+        // 이중 for문으로 scores 업데이트 
         for(int i = 0; i < answers.length; i++) {
-                if(answers[i] == patt1[index]) {
-                    maxOCnt++;
-                }
-                index++;
-                if (index == patt1.length) { // 인덱스가 배열 길이와 같아지면 0으로 초기화
-                    index = 0;
-                }
+    if(answers[i] == patt1[i % patt1.length]) scores[0]++;
+    if(answers[i] == patt2[i % patt2.length]) scores[1]++;
+    if(answers[i] == patt3[i % patt3.length]) scores[2]++;
+
         }
         
-        List<Integer> list1 = treeMap.getOrDefault(maxOCnt, new ArrayList());
-        list1.add(1);
-        treeMap.put(maxOCnt, list1);
-        biggest = Math.max(biggest, maxOCnt);
-        index = 0;
-        maxOCnt=0;
-
+        List<Integer> ansList = new ArrayList<>();
+        // 최대 정답의 개수 구하기 
+        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
-        for(int i = 0; i < answers.length; i++) {
-                if(answers[i] == patt2[index]) {
-                    maxOCnt++;
-                }
-                index++;
-                if (index == patt2.length) { // 인덱스가 배열 길이와 같아지면 0으로 초기화
-                    index = 0;
-                }
+        // max와 동일하면 +1해서 리스트에 추가하기 
+        for(int i = 0; i <scores.length; i++){
+            if(max == scores[i]) {
+                ansList.add(i+1);
+            }
         }
-        List<Integer> list2 = treeMap.getOrDefault(maxOCnt, new ArrayList());
-        list2.add(2); // 수포자 2
-        treeMap.put(maxOCnt, list2);
-        biggest = Math.max(biggest, maxOCnt);
-
-        index =0;
-        maxOCnt=0;
-
         
-        for(int i = 0; i < answers.length; i++) {
-                if(answers[i] == patt3[index]) {
-                    System.out.println("why...");
-                    maxOCnt++;
-                }
-                index++;
-                if (index == patt3.length) { // 인덱스가 배열 길이와 같아지면 0으로 초기화
-                    index = 0;
-                }
-        }
-         List<Integer> list3 = treeMap.getOrDefault(maxOCnt, new ArrayList());
-        list3.add(3); // 수포자 2
-
-        treeMap.put(maxOCnt, list3);
-                        biggest = Math.max(biggest, maxOCnt);
-
-        System.out.println(maxOCnt);
-        
-
-        List<Integer> ansList = treeMap.get(biggest);
-        int[] answer = new int[ansList.size()];
-        for(int i = 0; i < ansList.size(); i++) {
-            answer[i] = ansList.get(i);
-        }
-        return answer;
+        return ansList.stream()
+            .mapToInt(i -> i).toArray();
     }
 }
