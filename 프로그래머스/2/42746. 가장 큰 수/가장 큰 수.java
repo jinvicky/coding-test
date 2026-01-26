54cc6e1ae5699cdc1db7b8e8ba443f00ee1c5707
jinvicky <73690018+jinvicky@users.noreply.github.com>
2026-01-26 14:44:20 +0900

    [level 2] Title: 가장 큰 수, Time: 184.91 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,20 +1,18 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
 
-        // 1️⃣ int → String 변환
-        String[] arr = Arrays.stream(numbers)
-                             .mapToObj(String::valueOf)
-                             .toArray(String[]::new);
-
-        // 2️⃣ 커스텀 정렬
-        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
-
-        // 3️⃣ 0만 있는 경우 처리
-        if (arr[0].equals("0")) return "0";
-
-        // 4️⃣ 이어 붙이기
-        return String.join("", arr);
+      
+        return Arrays.stream(numbers)
+        .mapToObj(String::valueOf)
+        .sorted((s1, s2) -> {
+            int original = Integer.parseInt(s1+s2);
+            int reverse = Integer.parseInt(s2+s1);
+            return reverse - original;
+        })
+        .collect(Collectors.joining(""))
+        .replaceAll("^0+", "0");
     }
 }
