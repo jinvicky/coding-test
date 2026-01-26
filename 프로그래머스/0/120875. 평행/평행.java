aac662327dc4d86742163a505edee46c849052d1
jinvicky <73690018+jinvicky@users.noreply.github.com>
2026-01-26 18:29:15 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[][] dots;
+
+    public int solution(int[][] dots) {
+        this.dots = dots;
+        if (isParallel(0, 1, 2, 3)) return 1;
+        if (isParallel(0, 2, 1, 3)) return 1;
+        if (isParallel(0, 3, 1, 2)) return 1;
+        return 0;
+    }
+
+    boolean isParallel(int a, int b, int c, int d) {
+        int x = (dots[a][0] - dots[b][0]) * (dots[c][1] - dots[d][1]);
+        int y = (dots[a][1] - dots[b][1]) * (dots[c][0] - dots[d][0]);
+        return x == y;
+    }
+}
