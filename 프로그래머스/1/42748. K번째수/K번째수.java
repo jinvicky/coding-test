416f4c1a28307a879a58d134f832b051c7a1fffb
Jin Namkung <73690018+jinvicky@users.noreply.github.com>
2026-03-20 12:14:43 +0900

    [level 1] Title: K번째수, Time: 0.45 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,27 +1,22 @@
 import java.util.*;
 
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> ansList = new ArrayList<>();
-        
+        int[] answer = new int[commands.length];
         
         for(int i = 0; i < commands.length; i++) {
-            int[] sliced = Arrays.copyOfRange(array, commands[i][0]-1 , commands[i][1]);
-            Arrays.sort(sliced);
-
-            ansList.add(sliced[commands[i][2] - 1]);
+            int from = commands[i][0] - 1;
+            int to = commands[i][1] ;
+            int k = commands[i][2];
+            
+            int[] subArr = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(subArr);
+            
+            
+            
+            answer[i] = subArr[k-1];
         }
-        
-        // commands[i][2] 번째 수를 ansList에 넣기
-        
-        
-        
-        
-        
-        // 정답 리스트를 배열로 변환해서 리턴한다. 
-        int[] answer = ansList.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
 
         return answer;
     }
