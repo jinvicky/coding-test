8deae9b37876b0ba626bcb0271811eaa5aa83214
Jin Namkung <73690018+jinvicky@users.noreply.github.com>
2026-04-09 16:06:56 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int M, int N) {
+        if(M==1 && N==1) return 0;
+        
+        int answer = 0;
+        
+        
+        
+        return M * N - 1;
+    }
+}
