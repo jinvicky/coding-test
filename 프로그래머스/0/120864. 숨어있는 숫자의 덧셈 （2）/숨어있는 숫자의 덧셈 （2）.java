0f5765fd5e7aedc3ec7a4ad035476105ac1af953
Jin Namkung <73690018+jinvicky@users.noreply.github.com>
2026-04-10 14:04:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.07 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,26 +1,26 @@
 class Solution {
     public int solution(String my_string) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < my_string.length(); i++){
-            char c = my_string.charAt(i);
-
-            if(Character.isDigit(c)) {
-                sb.append(c);
-            } else {
-                if(sb.length() > 0) {
-                    answer += Integer.parseInt(sb.toString());
-                    sb.setLength(0); // 초기화
-                }
-            }
-        }
-
-        // 마지막 숫자 처리 (🔥 중요)
-        if(sb.length() > 0) {
-            answer += Integer.parseInt(sb.toString());
-        }
-
-        return answer;
+       // 숫자가 아니면 더하지 않는다. 
+       // 숫자라면 다음이 숫자가 아닐 때까지 sb를 사용해서 합친 후 더한다. 
+       // 마지막에 sb가 남아있는지 확인하고 남아 있으면 더한다. 
+        
+       StringBuilder sb = new StringBuilder();
+       int total = 0;
+       for(char c : my_string.toCharArray()) {
+           if(!Character.isDigit(c)) {
+               // 기존 sb를 total에 더하기 
+               if(sb.length() > 0) {
+                    total += Integer.parseInt(sb.toString());
+               }
+               // sb 초기화
+               sb = new StringBuilder();
+               continue;
+           }
+           sb.append(c);
+       }
+    
+        if(sb.length() > 0) total += Integer.parseInt(sb.toString());
+        
+       return total;
     }
 }
