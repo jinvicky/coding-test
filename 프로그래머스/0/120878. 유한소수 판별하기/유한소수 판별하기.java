f635a4367a600a40edf67286c38d04a8a4f612aa
Jin Namkung <73690018+jinvicky@users.noreply.github.com>
2026-04-10 15:54:39 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        int gcd = gcd(a, b);
+        b /= gcd;
+
+        while (b % 2 == 0) b /= 2;
+        while (b % 5 == 0) b /= 5;
+
+        return b == 1 ? 1 : 2;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
