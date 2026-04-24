a6621243c182889b0156416f559b2a0b27b9146d
Jin Namkung <73690018+jinvicky@users.noreply.github.com>
2026-04-24 15:00:45 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -5,21 +5,27 @@ class Solution {
         int limitX = board[0] / 2;
         int limitY = board[1] / 2;
         
-        for (int i = 0; i < keyinput.length; i++) {
+        for(int i = 0; i < keyinput.length; i++) {
             int nextX = answer[0];
             int nextY = answer[1];
             
-            switch (keyinput[i]) {
-                case "left": nextX--; break;
-                case "right": nextX++; break;
-                case "up": nextY++; break;
-                case "down": nextY--; break;
+            String dir = keyinput[i];
+            
+            if(dir.equals("left")) {
+                nextX--;
+            } else if (dir.equals("right")){
+                nextX++;
+            } else if (dir.equals("up")){
+                nextY++;
+            } else if (dir.equals("down")){
+                nextY--;
             }
             
-            if (Math.abs(nextX) <= limitX && Math.abs(nextY) <= limitY) {
+            if(Math.abs(nextX) <= limitX && Math.abs(nextY)  <= limitY) {
                 answer[0] = nextX;
                 answer[1] = nextY;
             }
+        
         }
         
         return answer;
